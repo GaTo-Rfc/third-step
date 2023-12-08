3430e4153eefbb0c57a3d24221d4ac37a15a2a51
reiya023 <xqmp71180@mineo.jp>
2023-12-08 21:19:58 +0900

    多分完成

--- statistics.rb
+++ statistics.rb
@@ -6,13 +6,13 @@ p english = [44, 87, 100, 63, 52, 60, 58, 73, 55, 86, 29, 56, 89, 23, 65, 84, 64
 # 平均
 scie_ave = science.sum.fdiv(science.length)
 eng_ave = english.sum.fdiv(english.length)
+
 puts "理科平均点 : #{scie_ave}"
 puts "英語平均点 : #{eng_ave}"
 
 # 標準偏差
 stddev_pre = science.map{|x| (x - scie_ave) ** 2}
 scie_stddev = Math.sqrt(stddev_pre.sum / science.length).round(2)
-
 stddev_pre = english.map{|x| (x - eng_ave) ** 2}
 eng_stddev = Math.sqrt(stddev_pre.sum / english.length).round(2)
 
@@ -24,10 +24,20 @@ puts "理科合計点 : #{science.sum}"
 puts "英語合計点 : #{english.sum}"
 
 # 偏差値
+scie_dev = []
+eng_dev = []
+for i in 0...20
+    scie_dev[i] = ((science[i] - scie_ave) / scie_stddev * 10 + 50).round(2)
+    eng_dev[i] = ((english[i] - eng_ave) / eng_stddev * 10 + 50).round(2)
+end
 
+puts "理科偏差値 :"
+p scie_dev
+puts "英語偏差値 :"
+p eng_dev
 
 # 並べ替え
-puts "science sort score :"
+puts "理科降順並べ替え :"
 p science.sort.reverse
-puts "english sort score :"
+puts "英語降順並べ替え :"
 p english.sort.reverse
